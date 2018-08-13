3ea018d6cb2f1cd7572066a6e84115fede279d03
Trevor Joynson (trevorj) <github@skywww.net>
2018-08-13 16:56:59 -0700

    only require cudart for now

--- tfdetect/cuda.py
+++ tfdetect/cuda.py
@@ -1,6 +1,7 @@
 CUDA_LIBS = {
     '9.0':
-        dict(cublas='9.0', cudnn='7', cufft='9.0', curand='9.0', cudart='9.0'),
+        # dict(cublas='9.0', cudnn='7', cufft='9.0', curand='9.0', cudart='9.0'),
+        dict(cudart='9.0'),
 }
 
 CURA_LIBS_MAP = {
